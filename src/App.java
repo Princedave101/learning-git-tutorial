8b69fc730525e5a54bc70ee67e79f7a1a598c867
PrinceDave101 <dpokeke@gmail.com>
2023-09-24 17:35:46 +0100

    Trying out binaryI/O classes in java

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        try (DataOutputStream outputStream = new DataOutputStream(new FileOutputStream("out.dat"))) {
+            outputStream.writeDouble(10.97);
+        }
+        try (DataInputStream inputStream = new DataInputStream(new FileInputStream("out.dat"))) {
+            System.out.println(inputStream.readDouble());
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Date;
+public class Main{
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException {
+        try{
+            try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("out.dat", true))) {
+                Integer n = Integer.valueOf(9);
+                
+                Date d = new Date();
+                out.writeObject(n);
+                out.writeObject(n);
+                out.writeObject(d);
+                out.writeObject(d);
+                out.writeObject(Integer.valueOf(7));
+                System.out.println("hello world!!");
+            } 
+
+            try( ObjectInputStream in = new ObjectInputStream(new FileInputStream("out.dat"))){
+                byte[] b = new byte[]{19, 9, 6};
+                in.read(b);
+                System.out.println(Arrays.toString(b));
+                Integer n1 = (Integer)in.readObject();
+                Integer n2 = (Integer)in.readObject();
+                Date d1  = (Date)in.readObject();
+                Date d2  = (Date)in.readObject();
+                Integer n3 = (Integer)in.readObject();
+                System.out.println(d1 + " "+ " "+ d2 );
+
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+        
+}
